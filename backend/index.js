53b7c190e199ce60a1c891d1e73ee710bf50b5ba
kushagra <kushagra17800@gmail.com~>
2023-07-16 17:06:54 +0530

    express created

--- backend/index.js
+++ backend/index.js
@@ -6,16 +6,10 @@ connectToMongo();
 const app = express();
 const port = 3000;
 
-// Middleware
-app.use(express.json()); // Parse JSON request bodies
-app.use(express.urlencoded({ extended: true })); // Parse URL-encoded request bodies
+// Available routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
-// Routes
-app.get('/', (req, res) => {
-  res.send('Hello, harry!');
-});
-
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
---
+++ backend/models/Notes.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new Schema({
+  title: {
+    type : String,
+    required : true
+  }, 
+  description :  {
+    type : String,
+    required : true,
+    unique : true
+  },
+
+  tag: {
+    type : String,
+    defult : "General"
+  },
+  date: {
+    type : Date,
+    required : Date.now
+  } 
+});
+
+
+module.exports = mongoose.model('notes', NotesSchema);
